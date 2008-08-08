2bb4b81d4f4f6fd9743e5c60e375628ff2fc044f
Eric Schulte <schulte.eric@gmail.com>
2008-08-08 14:41:22 -0700

    switching to using the find command

--- jump.el
+++ jump.el
@@ -1,6 +1,6 @@
-;;; jump.el --- jump between buffer
+;;; jump.el --- jump between buffers
 
-;;; Idea: to subsume ffip and toggle had a 
+;;; Idea: to subsume ffip and toggle, which were both big influences
 
 ;;; TODO:
 
@@ -23,7 +23,7 @@
 
 ;; things to keep
 
-;; - the idea of having a "root"
+;; - the idea of having a "root" (string or function)
 ;; - the ability to subsume ffip
 ;; - the bulk of rinari-jump "maybe with the addition of documentation
 ;;   string to rule sets"
@@ -32,19 +32,29 @@
 (require 'which-func)
 (require 'inflections) ;; try plural/singular versions by default
 
+(defvar jump-ignore-file-regexp
+  "\\\"\\(.*\\(git\\|svn\\|cvs\\).*\\|.*~\\|.*\\#.*\\#\\)\\\""
+  "regexp for the find shell command to ignore undesirable files")
+
+(defvar jump-root
+  nil
+  "Set to a path to override the `jump-root' function.")
+
 (defun jump-root (&optional dir)
   "Find the root of the project defined by presence of `.emacs-project'."
   (file-name-directory (plv-find-project-file default-directory "")))
 
 (defcustom jump-method-command
-  'which-function
+  'ruby-add-log-current-method
   "Function used to determine the current function.  Defaults to
 `which-function', but others may be preferable, for example
 `ruby-add-log-current-method' is more reliable in ruby code.")
 
 (defun jump-method ()
+  "Return the method defined at the current position in current
+buffer."
   (let ((func (funcall jump-method-command)))
-    (or (and (string-match "#\\(.+\\)" func) (match-string 1 func))
+    (or (and func (string-match "#\\(.+\\)" func) (match-string 1 func))
 	func)))
 
 (defun jump-create (path)
@@ -52,50 +62,117 @@
 missing."
   (find-file path))
 
-;; jump from a spot
+(defun flatten (input)
+  (if input
+      (if (consp input)
+	  (append (flatten (car input))
+		  (flatten (cdr input)))
+	(list input))))
+
+(defun jump-recursive-directory-files (directory)
+  (split-string (shell-command-to-string
+		 (concat "find " directory " -type f -not -regex "
+			 jump-ignore-file-regexp))))
+
+(defun jump-select-and-find-file (files)
+  "Select a single file from a list.  Return the path selected or
+nil if files was empty."
+  (let ((file   (case (length files)
+		  (0 nil)
+		  (1 (car files))
+		  (t (ido-completing-read "Jump to: " files)))))
+    (if file (find-file file))))
+
+(defun jump-to-file (&optional file)
+  "Open the file located at file if file ends in a / then look in
+the related directory, and if file contains regexps then select
+from all matches."
+  (interactive "Mfile: ")
+  (let ((default-directory (or jump-root
+			       (jump-root))))
+    (if (string-match "/$" file) ;; open directory
+	(ido-find-file-in-dir file)
+      (if (file-exists-p file) ;; open file
+	  (find-file file)
+	(jump-select-and-find-file ;; open with regexp
+	 (let ((base (reverse (split-string file "/"))) dir matcher)
+	   (delete-dups
+	    (loop for subdir in base ;; for incrementally less directories
+		  do (setf matcher (car base)) (setf base (cdr base))
+		  (setf dir (concat (mapconcat 'identity (reverse base) "/") "/"))
+		  collect (delq nil
+				(mapcar ;; for all files under directory
+				 (lambda (el)
+				   (if (string-match matcher el) ;; match regexp
+				       (concat dir "/" el)))
+				 (jump-recursive-directory-files dir)))))))))))
+
+(defun jump-to-method (&optional method)
+  "If `jump-method' returns method in buffer, go to the first
+line inside of method."
+  (interactive "Mmethod: ")
+  (goto-char (point-min))
+  (while (not (or (string-equal (jump-method) method)
+		  (and (> (forward-line 1) 0)
+		       (goto-char (point-min)))))))
+
+;; facade functions
 (defun jump-from (spec)
+  "jump from a spot"
   )
 
-;; jump to a spot
 (defun jump-to (spec)
+  "jump to a spot"
   )
 
-;; jump to a specified path (regexp OK)
 (defun jump-to-path (path)
   "Jump to the location specified by PATH (regexp allowed in
 path).  If path ends in / then just look in that directory"
-  (let ((default-directory (jump-root))
-	method method-point)
+  (let ((file path)
+	method)
     (when (string-match "^\\(.*\\)#\\(.*\\)$" path)
       (setf method (match-string 2 path))
-      (setf path (match-string 1 path)))
-    (if (if (string-match "/$" path)
-	    (ido-find-file-in-dir path)
-	  (if (file-exists-p path)
-	      (find-file path)
-	    (let ((base (reverse (split-string path "/"))) ;; search with regexp
-		  dir matcher files found-file)
-	      (while (and (null found-file) (consp base))
-		(setf matcher (car base)) (setf base (cdr base)) ;; drop down a dir
-		(setf dir (concat (mapconcat 'identity (reverse base) "/") "/"))
-		(setf files (delq nil (mapcar
-				       (lambda (file) ;; check for matches to regexp
-					 (if (string-match (concat "^" matcher) file)
-					     file))
-				       (directory-files dir))))
-		(case (length files)
-		  (0 )
-		  (1 (setf found-file (car files)))
-		  (t (setf found-file (ido-completing-read dir files)))))
-	      (if found-file (find-file (concat dir found-file))))))
-	(when method ;; if file was found, then find method
-	  (goto-char (point-min))
-	  (while (and (forward-line) (null method-point))
-	    (if (string-match method (or (jump-method) ""))
-		(setf method-point (point))))
-	  (if method-point
-	      (goto-char method-point)
-	    (goto-char (point-min)))))))
+      (setf file (match-string 1 path)))
+    (if (jump-to-file file)
+	(when method (jump-to-method method))
+      (message (format "no file found for %s" path)))))
+
+;; ;;; old version
+;; (defun jump-to-path (path)
+;;   "Jump to the location specified by PATH (regexp allowed in
+;; path).  If path ends in / then just look in that directory"
+;;   (let ((default-directory (jump-root))
+;; 	method method-point)
+;;     (when (string-match "^\\(.*\\)#\\(.*\\)$" path)
+;;       (setf method (match-string 2 path))
+;;       (setf path (match-string 1 path)))
+;;     (if (if (string-match "/$" path) ;; open directory
+;; 	    (ido-find-file-in-dir path)
+;; 	  (if (file-exists-p path) ;; open file
+;; 	      (find-file path)
+;; 	    (let ((base (reverse (split-string path "/"))) ;; search with regexp
+;; 		  dir matcher files found-file)
+;; 	      (while (and (null found-file) (consp base))
+;; 		(setf matcher (car base)) (setf base (cdr base)) ;; drop down a dir
+;; 		(setf dir (concat (mapconcat 'identity (reverse base) "/") "/"))
+;; 		(setf files (delq nil (mapcar
+;; 				       (lambda (file) ;; check for matches to regexp
+;; 					 (if (string-match (concat "^" matcher) file)
+;; 					     file))
+;; 				       (directory-files dir))))
+;; 		(case (length files)
+;; 		  (0 )
+;; 		  (1 (setf found-file (car files)))
+;; 		  (t (setf found-file (ido-completing-read dir files)))))
+;; 	      (if found-file (find-file (concat dir found-file))))))
+;; 	(when method ;; if file was found, then find method
+;; 	  (goto-char (point-min))
+;; 	  (while (and (forward-line) (null method-point))
+;; 	    (if (string-match method (or (jump-method) ""))
+;; 		(setf method-point (point))))
+;; 	  (if method-point
+;; 	      (goto-char method-point)
+;; 	    (goto-char (point-min)))))))
 
 ;; handle the jump
 (defun jumper ())
--- test/init.el
+++ test/init.el
@@ -1,47 +1,116 @@
 (cd (file-name-directory (or load-file-name buffer-file-name)))
 (add-to-list 'load-path "../")
 (add-to-list 'load-path "./")
-(add-to-list 'load-path "~/emacs/loads/ruby-mode")
+(add-to-list 'load-path "~/emacs/elisp/ruby-mode")
 (require 'jump)
 (require 'elunit)
 (require 'ido)
 (require 'ruby-mode)
 (ido-mode t)
 
-;; test jumping
+;;--------------------------------------------------------------------------------
+;; test suite
 (defsuite jump-suite nil
-  :setup-hook (lambda () )
+  :setup-hook (lambda ()
+		(setq jump-root "~/projects/jump/test/jump-fake-app/"
+		      default-directory "~/projects/jump/test/jump-fake-app/"
+		      jump-method-command 'ruby-add-log-current-method
+		      jump-method-placements
+		      '(("foods/pork.rb" "cook_stomach" 32)
+			("foods/pork.rb" "cook_butt" 69)
+			("foods/pork.rb" "cook_outer_back" 117)
+			("foods/pork.rb" "cook_inner_back" 163))
+		      jump-path-regexps
+		      '(("pork.rb" "foods/pork.rb")
+			("pork.rb" "foods/")
+			;; ("pork.rb" "foods/.*")
+			("pig.rb" "animals/pig.rb")
+			;; ("pig.rb" "animals/.*g.rb")
+			;; ("pig.rb" ".*/pig.rb")
+			("chicken.rb" "animals/chicken.rb")
+			;; ("chicken.rb" "animals/........rb")
+			;; ("chicken.rb" "animals/chi.+.rb")
+			)
+		      ))
   :teardown-hook (lambda ()
 		   (switch-to-buffer "*Messages*")
 		   (message "test completed")))
 
-(deftest jump-to-path-test jump-suite
-  ;; test moving from everywhere to everywhere
-  (save-excursion
-    (unless (y-or-n-p "allways select the first option ok? ")
-      (error "if you won't co-operate, I won't run these tests"))
-    (cd "./jump-fake-app")
-    (flet ((jump-root () "~/projects/jump/test/jump-fake-app/")
-	   (jump-method () (ruby-add-log-current-method))
-	   (jumpit (path end)
-		   ;; go there
-		   (jump-to-path path)
-		   ;; assert where there is
-		   (message (format "asserting at %S" end))
-		   (assert-equal (file-name-nondirectory (car end))
-				 (file-name-nondirectory (buffer-file-name)))
-		   (assert-equal (cdr end) (point))
-		   ;; clean up
-		   (kill-buffer (file-name-nondirectory (car end)))))
-      (jumpit "animals/.*" '("chicken.rb" . 1))
-      (jumpit "animals/pig.rb#stomach" '("pig.rb" . 28))
-      ;; - failing this test because it needs to start looking from the jump-root
-      ;; - also, needs to gracefully fail (return nil) when nothing matches the regexp
-      (jumpit ".*pork.rb#cook_butt" '("pork.rb" . 69))
-      )))
+;;--------------------------------------------------------------------------------
+;; tests
+(deftest jump-to-file-test jump-suite
+  (message "testing that jump-to-file lands in the correct file")
+  (flet ((check-file-after-jump (file regexp)
+				(message (format "%s =~ %s" file regexp))
+				(jump-to-file regexp)
+				(assert-equal (file-name-nondirectory file) file)
+				(kill-buffer (buffer-name))))
+    (mapcar (lambda (el)
+	      (apply 'check-file-after-jump el))
+	    jump-path-regexps)))
 
-;; simple test
-;; (flet ((jump-root () "~/projects/jump/test/jump-fake-app/"))
-;;   (jump-to-path "foods/.*.rb"))
+;; (deftest jump-method-test jump-suite
+;;   (message "testing that jump-method returns the correct method")
+;;   (flet ((check-method-at-place (file method target)
+;; 				(find-file file)
+;; 				(goto-char target)
+;; 				(assert-equal (jump-method) method)
+;; 				(message (format "%s" (thing-at-point 'line)))
+;; 				(kill-buffer (file-name-nondirectory file))))
+;;     (mapcar (lambda (el)
+;; 	      (apply 'check-method-at-place el))
+;; 	    jump-method-placements)))
 
+;; (deftest jump-to-method-test jump-suite
+;;   (message "testing jump-to-method")
+;;   (flet ((jump-and-check (file method target)
+;; 			 (find-file file)
+;; 			 (jump-to-method method)
+;; 			 (assert-equal target (point))
+;; 			 (message (format "%s" (thing-at-point 'line)))
+;; 			 (kill-buffer (file-name-nondirectory file))))
+;;     (mapcar (lambda (el)
+;; 	      (apply 'jump-and-check el))
+;; 	    jump-method-placements)))
+
+;; (deftest jump-to-path-test jump-suite
+;;   (message "testing jump-to-path")
+;;   (flet ((jump-to-and-check (file method target)
+;; 			    (let ((path (concat file "#" method)))
+;; 			      ;; jump to directory
+;; 			      ;; jump to file
+;; 			      ;; jump to file#method
+;; 			      ;; jump to file#method w/regexp
+;; 			      )))
+;;     (mapcar (lambda (el)
+;; 	      (apply 'jump-and-check el))
+;; 	    jump-method-placements)))
+
+;; (deftest jump-to-path-test jump-suite
+;;   ;; test moving from everywhere to everywhere
+;;   (save-excursion
+;;     (unless (y-or-n-p "allways select the first option ok? ")
+;;       (error "if you won't co-operate, I won't run these tests"))
+;;     (cd "./jump-fake-app")
+;;     (flet ((jump-root () "~/projects/jump/test/jump-fake-app/")
+;; 	   (jump-method () (ruby-add-log-current-method))
+;; 	   (jumpit (path end)
+;; 		   ;; go there
+;; 		   (jump-to-path path)
+;; 		   ;; assert where there is
+;; 		   (message (format "asserting at %S" end))
+;; 		   (assert-equal (file-name-nondirectory (car end))
+;; 				 (file-name-nondirectory (buffer-file-name)))
+;; 		   (assert-equal (cdr end) (point))
+;; 		   ;; clean up
+;; 		   (kill-buffer (file-name-nondirectory (car end)))))
+;;       (jumpit "animals/.*" '("chicken.rb" . 1))
+;;       (jumpit "animals/pig.rb#stomach" '("pig.rb" . 28))
+;;       ;; - failing this test because it needs to start looking from the jump-root
+;;       ;; - also, needs to gracefully fail (return nil) when nothing matches the regexp
+;;       (jumpit ".*pork.rb#cook_butt" '("pork.rb" . 69))
+;;       )))
+
+;;--------------------------------------------------------------------------------
+;; run tests
 (elunit "jump-suite")
